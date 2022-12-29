53dc89021477f35395b20e57b5a29e65547f3f91
vamsi-012 <vamshikatikala1@gmail.com>
2022-12-29 13:11:53 +0000

    fetch and routing

---
+++ src/components/BlogList/index.js
@@ -0,0 +1,51 @@
+// Write your JS code here
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+
+import BlogItem from '../BlogItem'
+import './index.css'
+
+class BlogList extends Component {
+  state = {
+    blogsData: [],
+    isLoading: true,
+  }
+
+  componentDidMount() {
+    this.getBlogsList()
+  }
+
+  getBlogsList = async () => {
+    const response = await fetch('https://apis.ccbp.in/blogs')
+    const data = await response.json()
+    const updatedDataList = data.map(each => ({
+      id: each.id,
+      title: each.title,
+      imageUrl: each.image_url,
+      avatarUrl: each.avatar_url,
+      author: each.author,
+      topic: each.topic,
+    }))
+
+    this.setState({blogsData: updatedDataList, isLoading: false})
+  }
+
+  render() {
+    const {blogsData, isLoading} = this.state
+    return (
+      <div className="blog-list-container">
+        {isLoading ? (
+          <div // testid="loader">
+            <Loader type="TailSpin" color="#00BFFF" height={50} width={50} />
+          </div>
+        ) : (
+          blogsData.map(item => <BlogItem blogData={item} key={item.id} />)
+        )}
+      </div>
+    )
+  }
+}
+
+export default BlogList
